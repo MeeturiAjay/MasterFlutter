dcbc71154edd254dded412eb65aa723b9151627b
MeeturiAjaykumar <meeturiajaykumar.23@gmail.com>
2024-05-01 18:53:37 +0530

    day2 completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+        title: const Text("Master Flutter"),
+      ),
+      drawer: const Drawer(
+        backgroundColor: Colors.indigo,
+      ),
+      body: const Center(
+        child: Text("Welcome to Master Flutter in 8hrs course"),
+      ),
+    );
+  }
+}
