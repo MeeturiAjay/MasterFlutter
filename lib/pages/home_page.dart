902ed66f4fb8ce499798f23fb7f81b24f5b58976
MeeturiAjaykumar <meeturiajaykumar.23@gmail.com>
2024-05-05 08:52:34 +0530

    DAY13 completed(picking up the json's data and decoding)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:masterflutter/models/catalog.dart';
 import 'Item_widget.dart';
 import 'drawer.dart'; // Importing the drawer.dart file
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}); // Fixed super.key to Key? key
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+  }
+
+  // Fixed super.key to Key? key
   @override
   Widget build(BuildContext context) {
     final DummyList = List.generate(20, (index) => CatalogModels.items[0]);
