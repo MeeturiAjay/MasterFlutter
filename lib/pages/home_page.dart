ac6e4c238fd18dc2ebf393ae0e1cf293f793e220
MeeturiAjaykumar <meeturiajaykumar.23@gmail.com>
2024-05-05 12:38:37 +0530

    DAY15,16 completed (GridView and GridTile).

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:masterflutter/models/catalog.dart';
-import 'Item_widget.dart';
+import 'item_widget.dart'; // Changed to lowercase
 import 'drawer.dart'; // Importing the drawer.dart file
 import 'dart:convert';
 
@@ -36,21 +36,54 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // final DummyList = List.generate(20, (index) => CatalogModels.items[0]);
     return Scaffold(
-        appBar: AppBar(
-          //backgroundColor: Colors.indigo,
-          title: const Text("Master Flutter"),
-        ),
-        drawer: Mydrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: (CatalogModels.items != null && CatalogModels.items.isNotEmpty)? ListView.builder(
-            itemCount: CatalogModels.items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                item: CatalogModels.items[index],
-              );
-            },
-          ): const Center(child: CircularProgressIndicator()),
-        ));
+      appBar: AppBar(
+        //backgroundColor: Colors.indigo,
+        title: const Text("Master Flutter"),
+      ),
+      drawer: Mydrawer(), // Changed to MyDrawer
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModels.items != null && CatalogModels.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (BuildContext context, int index) {
+                  // Added parameter names
+                  final item = CatalogModels.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(color: Colors.indigo),
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      footer: Container(
+                        padding: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                            color: Colors.indigo
+                          ),
+                          child: Text(
+                            style: const TextStyle(
+                              color: Colors.white
+                            ),
+                              "\$${item.price.toString()}")),
+                      // Changed to Text widget
+                      child: Image.network(item.image),
+                    ),
+                  );
+                },
+                itemCount: CatalogModels.items.length,
+              )
+            : const Center(child: CircularProgressIndicator()),
+      ),
+    );
   }
 }
